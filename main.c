11f39c9ae8b9036cc028e894db6fee423a35e10e
rnin9 <rkdalswn1209@kw.ac.kr>
2020-05-08 00:48:02 +0900

    Edit tm pointer

--- main.c
+++ main.c
@@ -6,7 +6,7 @@
 #include <stdlib.h>
 
 struct stat stat1, stat2;
-struct tm *time1, *time2;
+struct tm time1, time2;
 
 void filestat1();
 void filestat2();
@@ -54,24 +54,27 @@ void filestat2(){
 
 //파일 1의 시간 정보를 가져오는 함수 작성
 void filetime1(){
-    time1 = localtime(&stat1.st_mtime);
-    if(time1 == NULL){
-        printf("Get File1 Time Error!\n");
+    struct tm* timecheck;
+    timecheck = localtime(&stat1.st_mtime);
+    if(timecheck == NULL){
         exit(0);
     } 
     else{
+    time1 = *localtime(&stat1.st_mtime);
         return;
     }
 }
 
 //파일 2의 시간 정보를 가져오는 함수 작성
 void filetime2(){
-    time2 = localtime(&stat2.st_mtime);
-    if(time2 == NULL){
+    struct tm* timecheck;
+    timecheck = localtime(&stat2.st_mtime);
+    if(timecheck == NULL){
         printf("Get File2 Time Error!\n");
         exit(0);
     } 
     else{
+    time2 = *localtime(&stat2.st_mtime);
         return;
     }
 }
